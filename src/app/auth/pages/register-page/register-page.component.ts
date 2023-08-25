04b2a68cc9b81a6b0c1c16947d26175cb993ea68
Andreea <andelenasamur@gmail.com>
2023-08-25 13:45:21 +0200

    create register form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [ Validators.required, Validators.email ] ],
+    password: ['', [ Validators.required, Validators.minLength(6) ] ],
+    name: ['', [ Validators.required ] ],
+    surname: ['', [ Validators.required ] ],
+  })
+
+  register() {
+    console.log(this.myForm.value)
+  }
+
 
 }
