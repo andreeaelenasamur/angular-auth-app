a252df90b9846d6116810c25e064a2ac0c6dba75
Andreea <andelenasamur@gmail.com>
2023-08-28 11:23:36 +0200

    error handling

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,14 +11,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [ Validators.required, Validators.email ]],
-    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    email: ['fernando@google.com', [ Validators.required, Validators.email ]],
+    password: ['1234567', [ Validators.required, Validators.minLength(6) ]],
   })
 
   login() {
-    console.log(this.myForm.value)
+    const { email, password } = this.myForm.value;
+
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => console.log('Todo bien!'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error')
+        }
+      })
   }
 
 }
